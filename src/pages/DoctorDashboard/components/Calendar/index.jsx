17e6ab234459e90d4255885a57e65f272506738e
edgard <edgard.habanbou@hotmail.com>
2023-12-05 21:57:43 +0200

    Added Add-apointment UI

--- src/pages/DoctorDashboard/components/Calendar/index.jsx
+++ src/pages/DoctorDashboard/components/Calendar/index.jsx
@@ -1,12 +1,68 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { axiosPost } from "../../../../Assets/api";
+
+import { setHours, setMinutes } from "date-fns";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./styles.css";
 
 const MyCalendar = () => {
-  const [appointmentDate, setAppointmentDate] = useState(new Date());
+  const includeTimes = [
+    setHours(setMinutes(new Date(), 0), 8),
+    setHours(setMinutes(new Date(), 30), 8),
+    setHours(setMinutes(new Date(), 0), 9),
+    setHours(setMinutes(new Date(), 30), 9),
+    setHours(setMinutes(new Date(), 0), 10),
+    setHours(setMinutes(new Date(), 30), 10),
+    setHours(setMinutes(new Date(), 0), 11),
+    setHours(setMinutes(new Date(), 30), 11),
+    setHours(setMinutes(new Date(), 0), 12),
+    setHours(setMinutes(new Date(), 30), 12),
+    setHours(setMinutes(new Date(), 0), 13),
+    setHours(setMinutes(new Date(), 30), 13),
+    setHours(setMinutes(new Date(), 0), 14),
+    setHours(setMinutes(new Date(), 30), 14),
+    setHours(setMinutes(new Date(), 0), 15),
+    setHours(setMinutes(new Date(), 30), 15),
+    setHours(setMinutes(new Date(), 0), 16),
+    setHours(setMinutes(new Date(), 30), 16),
+    setHours(setMinutes(new Date(), 0), 17),
+  ];
+
+  const [appointmentDateStart, setAppointmentDateStart] = useState(new Date());
+  const [appointmentDateEnd, setAppointmentDateEnd] = useState(new Date());
+  const [selectedPatient, setSelectedPatient] = useState("");
+  const [patients, setPatients] = useState([]);
+
+  const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours() % 12;
+    const minutes = date.getMinutes();
+    return `${month}-${day}-${year} ${hours}:${minutes}`;
+  };
+
+  const handleAddEvent = () => {
+    console.log("Start Date:", formatDate(appointmentDateStart));
+    console.log("End Date:", formatDate(appointmentDateEnd));
+    console.log("Selected Patient:", selectedPatient);
+  };
+
+  useEffect(() => {
+    const API_BASE_URL =
+      "http://localhost/hospital-managment-system/backend/api";
+    const PATIENTS_API_URL = `${API_BASE_URL}/patients/crud.php`;
+
+    const getPatients = () => {
+      axiosPost(PATIENTS_API_URL, "getAllPatients").then((response) => {
+        setPatients(response.data.patients);
+      });
+    };
+    getPatients();
+  }, []);
   return (
-    <div className="full-height full-width calendar-wrapper">
+    <div className=" calendar-wrapper">
       <div className="calendar">
         <div className="timeline">
           <div className="spacer"></div>
@@ -93,36 +149,64 @@ const MyCalendar = () => {
       </div>
       <div className="flex gap column">
         <div>
-          <h2>Add Event</h2>
+          <h2>Add Appointment</h2>
         </div>
         <div className="flex gap">
           <div className="flex column">
-            <label for="date_picker">Date:</label>
+            <label htmlFor="date_picker_start">Start Date:</label>
 
             <DatePicker
-              id="date_picker"
+              id="date_picker_start"
+              selected={appointmentDateStart}
               className="input"
-              selected={appointmentDate}
               onChange={(date) => {
-                setAppointmentDate(date);
-                console.log(date);
+                setAppointmentDateStart(date);
               }}
-              timeInputLabel="Time:"
-              dateFormat="MM/dd/yyyy h:mm aa"
-              showTimeInput
+              showTimeSelect
+              includeTimes={includeTimes}
+              dateFormat="MMMM d, yyyy h:mm aa"
             />
           </div>
           <div className="flex column">
-            <label for="event_name">Event Name:</label>
-            <input
-              type="text"
+            <label htmlFor="date_picker_end">End Date:</label>
+
+            <DatePicker
+              id="date_picker_end"
+              selected={appointmentDateEnd}
               className="input"
-              id="event_name"
-              placeholder="Event Name"
+              onChange={(date) => {
+                setAppointmentDateEnd(date);
+              }}
+              showTimeSelect
+              includeTimes={includeTimes}
+              dateFormat="MMMM d, yyyy h:mm aa"
             />
           </div>
+          <div className="flex column">
+            <label htmlFor="event_name">Patient:</label>
+
+            <select
+              value={selectedPatient}
+              className="input"
+              onChange={(e) => {
+                setSelectedPatient(e.target.value);
+              }}
+            >
+              <option disabled value="">
+                Select Patient
+              </option>
+
+              {patients?.map((patient, i) => (
+                <option key={i} value={patient.patient_id}>
+                  {patient.fname + " " + patient.lname}
+                </option>
+              ))}
+            </select>
+          </div>
           <div className="flex column right">
-            <button className="btn">Add Event</button>
+            <button className="btn" onClick={handleAddEvent}>
+              Add Event
+            </button>
           </div>
         </div>
       </div>
